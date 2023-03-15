5792eb5567a591f30ce51dbb1fb9b54ddf046568
Chris Manus <christtroopermanus106@student.unsrat.ac.id>
2023-03-15 15:52:48 +0800

    [FEAT] Menambahkan Fitur

---
+++ assets/js/jankenpon.js
@@ -0,0 +1,10 @@
+let response = prompt("Pilih 1. Gunting, 2. Kertas, dan 3. Batu");
+if (response == 1) {
+  alert("Anda Menang!");
+} else if (response == 2) {
+  alert("Anda Kalah!");
+} else if (response == 3) {
+  alert("Draw!");
+} else {
+  alert("Anda salah memasukkan angka!");
+}
