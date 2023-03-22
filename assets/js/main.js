3f839c956c136e8d3d3d38149f3904fee0dff034
Chris Manus <christtroopermanus106@student.unsrat.ac.id>
2023-03-22 21:44:27 +0800

    [FEAT] Menambahkan data pada API

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,34 @@ addToButton.addEventListener("click", function () {
     toDoContainer.removeChild(paragraph);
   });
 });
+
+let data = {
+  name: "Christtrooper Manus",
+  id: "01",
+  univ: "Sam Ratulangi University",
+  email: "christtroopermanus106@student.unsrat.ac.id",
+
+  name: "Ridel Kapulang",
+  id: "02",
+  univ: "Manado University",
+  email: "ridelkapulang@student.unima.ac.id",
+};
+
+const fetchOptions = {
+  method: "POST", // Jika mau diupdate maka diganti dengan "PUT" jika mau mengambil data bisa diganti dengan "GET"
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(data),
+};
+
+fetch(
+  "https://crudcrud.com/api/25ac18915e684d36b03f757c65f41de5/toDoList",
+  fetchOptions
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+// fetch("https://crudcrud.com/api/cbb10374adb54063b115bd051d14d6cd/toDoList")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
