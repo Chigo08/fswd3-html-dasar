03473d0ba7bf8aa489b4b2deef8267bcc43f07a3
Chris Manus <christtroopermanus106@student.unsrat.ac.id>
2023-03-22 22:47:29 +0800

    [FIX] memperbaiki kode

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,33 +16,73 @@ addToButton.addEventListener("click", function () {
   });
 });
 
-let data = {
-  name: "Christtrooper Manus",
-  id: "01",
-  univ: "Sam Ratulangi University",
-  email: "christtroopermanus106@student.unsrat.ac.id",
+// API
 
-  name: "Ridel Kapulang",
-  id: "02",
-  univ: "Manado University",
-  email: "ridelkapulang@student.unima.ac.id",
-};
-
-const fetchOptions = {
-  method: "POST", // Jika mau diupdate maka diganti dengan "PUT" jika mau mengambil data bisa diganti dengan "GET"
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-};
-
-fetch(
+customFetch(
   "https://crudcrud.com/api/25ac18915e684d36b03f757c65f41de5/toDoList",
-  fetchOptions
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  "GET"
+);
 
-// fetch("https://crudcrud.com/api/cbb10374adb54063b115bd051d14d6cd/toDoList")
+// fetch("https://crudcrud.com/api/25ac18915e684d36b03f757c65f41de5/toDoList")
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
+
+function customFetch(url, type, data) {
+  if (type === "GET") {
+    fetch(url, {
+      method: type,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Request Sucess!");
+        } else {
+          console.log("Request Failed!");
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }
+
+  if (type === "POST" || type == "PUT") {
+    fetch(url, {
+      method: type,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Request Sucess!");
+        } else {
+          console.log("Request Failed!");
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }
+
+  if (type === "DELETE") {
+    fetch(url, {
+      method: type,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Request Sucess!");
+        } else {
+          console.log("Request Failed!");
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+}
